6593b2b676fdb1a5de67eaeea0fa74f9ad9f7bc7
Velmurdov <batuvalll@gmail.com>
2023-08-06 10:59:44 +0300

    js3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
 
+import throttle from "lodash.throttle"
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    inputEmail: document.querySelector('[name = email]'),
+    textarea: document.querySelector('[name = message]')
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formdata = {};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(onInput), 500);
+
+loadDataFromLocalStorage();
+
+function onFormSubmit(event){
+    event.preventDefault();
+    event.currentTarget.reset();
+
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    
+    console.log(savedData);
+    console.log('email:', savedData.email);
+    console.log('message:', savedData.message);
+
+    localStorage.removeItem(STORAGE_KEY);
+
+};
+
+function onInput(event){
+    formdata[event.target.name] = event.target.value ;
+    const formDataStrf = JSON.stringify(formdata);
+    localStorage.setItem(STORAGE_KEY, formDataStrf);
+
+};
+
+function loadDataFromLocalStorage(){
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(savedData){
+        refs.inputEmail.value = savedData.email;
+        refs.textarea.value = savedData.message;
+    }
+};
